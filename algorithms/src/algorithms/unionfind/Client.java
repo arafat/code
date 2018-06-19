4c8a6218ac405e1c51c82503becc947e1e0c4aeb
Arafat Eajaz <arafat@arafat-linux.gur.corp.google.com>
2018-06-19 14:43:45 +0530

    .

---
+++ algorithms/src/algorithms/unionfind/Client.java
@@ -0,0 +1,16 @@
+/*
+ * Client class to test algorithm implementation.
+ */
+package algorithms.unionfind;
+
+/**
+ *
+ * Client class to test implementation.
+ */
+public class Client {
+    
+    public static void main(String[] args) {
+        System.out.println("Here is my client.");
+    }
+    
+}
--- algorithms/src/algorithms/unionfind/QuickFind.java
+++ algorithms/src/algorithms/unionfind/QuickFind.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Quick find is a varient of Union-Find algorithm where Find operation
+ * is optimized.
+ * Complexity Analysis
  */
 package algorithms.unionfind;
 
 /**
- *
- * @author arafat
+ * It maintains a set of connected components, a connected component is a
+ * graph with connected nodes, if 2 nodes in question belongs to same connected
+ * components then we can say that they are connected or reachable else not.
+ * 
+ * All the nodes are maintained as an array with array indeces representing the
+ * nodes and array values are used to create and maintain connected components.
+ * All nodes which belong to the same connected component will have same value
+ * in the array, so every Union operation will involve changing array entries
+ * to maintain connected components.
+ * On the other hand Find operation involves on check if 2 nodes have same value
+ * in array if they have then they are connected else not.
  */
 public class QuickFind {
+    
+    QuickFind() {
+    }
     
 }
